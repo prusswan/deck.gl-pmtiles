2c50e23903a130e0982ea275cb78cf90099674d1
prusswan <prusswan@gmail.com>
2024-10-03 21:57:43 +0800

    resolved errors with @loaders.gl/worker-utils and process.env (for non-node, iife build)

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
 import typescript from "@rollup/plugin-typescript";
+import alias from '@rollup/plugin-alias';
+import replace from '@rollup/plugin-replace';
+
+// from https://github.com/rollup/plugins/issues/440
+const addSyntheticNamedExportsToSkippedNodeImports = () => ({
+  load: (importee) => {
+    if (importee === '\u0000node-resolve:empty.js') {
+      return {code: 'export default {};', syntheticNamedExports: true};
+    } else {
+      return null;
+    }
+  }
+});
 
 export default [
   // common js
@@ -37,20 +50,32 @@ export default [
       // output.file must be under compilerOptions.outDir, see https://github.com/rollup/plugins/issues/243
       file: 'example/public/dist/bundle.js',
       format: 'iife',
+      sourcemap: true,
       name: 'PMTLayerLibrary',
       globals: {
         '@deck.gl/core': 'deck',
         '@deck.gl/layers': 'deck',
-        //'@luma.gl/core': 'luma'
+        '@luma.gl/core': 'luma'
       }
     },
-    external: ['@deck.gl/core', '@deck.gl/layers'], //'@luma.gl/core'],
+    external: ['@deck.gl/core', '@deck.gl/layers', '@luma.gl/core'],
     plugins: [
+      typescript(),
+      alias({
+        entries: {}
+      }),
+      addSyntheticNamedExportsToSkippedNodeImports(),
+      // for loaders.gl on non-node/iife builds, see https://github.com/visgl/loaders.gl/issues/2000
       resolve({
         browser: true,
-        preferBuiltins: false
+        preferBuiltins: false,
+        mainFields: ["browser", "main"]
       }),
-      typescript(),
+      // process.env not resolvable in browser, see https://github.com/rollup/rollup/issues/487
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+        __buildDate__: () => JSON.stringify(new Date()),
+      }),
       commonjs()
     ]
   }
